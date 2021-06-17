4dbc3099362a0edd252e18f3da310091dc4a0d37
935371670@qq.com <88107789a>
2021-06-17 20:55:31 +0800

    init

---
+++ eshop-inventory/src/main/java/com/zhss/eshop/InventoryApplication.java
@@ -0,0 +1,33 @@
+package com.zhss.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.zhss.eshop.common.config.DruidDataSourceConfig;
+import com.zhss.eshop.inventory.mq.MessageService;
+
+/**
+ * 系统启动类
+ * @author zhonghuashishan
+ *
+ */
+@SpringBootApplication
+@EnableScheduling
+@ServletComponentScan
+@EnableEurekaClient
+@EnableFeignClients
+@EnableBinding(MessageService.class)
+@Import(DruidDataSourceConfig.class)
+public class InventoryApplication {
+	
+	public static void main(String[] args) { 
+		SpringApplication.run(InventoryApplication.class, args);
+	}
+	
+}
